2cdfd7e333f28853b2e7aacc0a4ece1099d14ae6
Alvin Yudha Eka P <alvinyudha73@gmail.com>
2026-02-18 18:17:02 +0700

    update css

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
 const menu = document.querySelector('.menu');
-const navDropdown = document.querySelector('.nav-dropdown');
+const navDropdown = document.querySelector('.dropbtn');
 const iconBars = document.querySelector('.icon-bars');
 const iconClose = document.querySelector('.icon-close');
 
 function displayMenu() {
     if(menu.classList.contains('show')) {
         menu.classList.remove('show');
-        iconBars.style.display = 'inline';
-        iconClose.style.display = 'none';
+      
     } else {
         menu.classList.add('show');
-        iconBars.style.display = 'none';
-        iconClose.style.display = 'inline';
     }
 }
